55bb16523f3500f423a207daa83ec0853c9fa64e
Varsha Kadge <varshakadge@Varshas-Air.lan>
2026-04-05 21:14:27 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.use("/",(req, res) => {
+ res.send("hello from dashboard")
+})
+app.use("/hello",(req, res) => {
+ res.send("hello hello hello...")
+})
+app.use("/test",(req, res) => {
+ res.send("hello from server")
+})
+app.listen(3000,()=>{
+    console.log("server started successfully on port 3000...")
+});
